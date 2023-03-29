4bf23342c8653b2ef8d38c2c88f8ae4f21ab73be
Thibaut Septon <tsepton@gmail.com>
2023-03-29 17:55:00 +0200

    Update HTML info

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,14 @@
 import { Navbar } from "flowbite-react";
 import trans from "../translations/translator";
 
-export default function NavBar() {
+export default function NavBar(props: any) {
   return (
-    <Navbar fluid={true} rounded={true}>
+    <Navbar className={props.className} fluid={true} rounded={true} >
       <Navbar.Brand href="/">
         <img
           src="/img/logo.jpg"
           className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
+          alt="JFD forage Logo"
         />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           JFD forage
