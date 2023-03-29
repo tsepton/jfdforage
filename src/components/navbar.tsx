95bdb5cd30e05f4269b4f84beb352d06ce559c62
Thibaut Septon <tsepton@gmail.com>
2023-03-29 19:48:01 +0200

    Start implementing navbar smooth scroll

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,26 @@ import trans from "../translations/translator";
 
 export default function NavBar(props: any) {
 
-  // TODO active logic   :    active={true}
+  const getCurrentSection = () => {
+    // FIXME : window is only available at runtime => Use useState from react
+    // const uri = location.href.split("#");
+    // console.assert(uri.length === 2);
+    // return uri[1];
+    return ("#");
+  };
 
+  const scrollHandle = (e: any) => {
+    // TODO : adapt me, make it work
+    e.preventDefault();
+    console.log(e);
+    let href = e.target.href;
+    let position = document.getElementById(href.slice(0, href.length - 1)); //removing extra last - (dash)
+    window.location.href = "#" + href.slice(0, href.length - 1); // changing the url
+    position && position.scrollIntoView({ behavior: "smooth", block: "start" }); //scrolling the page
+  };
+
+  // TODO 
+  const isActive = (uri: string) => getCurrentSection() === uri;
 
   return (
     <Navbar className={props.className} fluid={true} rounded={true} >
@@ -21,11 +39,11 @@ export default function NavBar(props: any) {
       </Navbar.Brand>
 
       <Navbar.Collapse className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        <Navbar.Link active={true} href="#">{trans.get("navbar.home")}</Navbar.Link>
-        <Navbar.Link href="#presentation">{trans.get("navbar.presentation")}</Navbar.Link>
-        <Navbar.Link href="#geothermal">{trans.get("navbar.geothermal")}</Navbar.Link>
-        <Navbar.Link href="#services">{trans.get("navbar.services")}</Navbar.Link>
-        <Navbar.Link href="#pic">{trans.get("navbar.pic")}</Navbar.Link>
+        <Navbar.Link active={isActive("#")} href="#" onClick={scrollHandle}>{trans.get("navbar.home")}</Navbar.Link>
+        <Navbar.Link active={isActive("#presentation")} href="#presentation" onClick={scrollHandle}>{trans.get("navbar.presentation")}</Navbar.Link>
+        <Navbar.Link active={isActive("#geothermal")} href="#geothermal" onClick={scrollHandle}>{trans.get("navbar.geothermal")}</Navbar.Link>
+        <Navbar.Link active={isActive("#services")} href="#services" onClick={scrollHandle}>{trans.get("navbar.services")}</Navbar.Link>
+        <Navbar.Link active={isActive("#photo")} href="#photo" onClick={scrollHandle}>{trans.get("navbar.pic")}</Navbar.Link>
       </Navbar.Collapse>
 
       <div className="flex md:order-2 p-0" >
