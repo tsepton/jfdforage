35919f68e46342f5d28f3771d397bb896087c36d
Thibaut Septon <tsepton@gmail.com>
2023-03-29 18:34:52 +0200

    Optimize imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import NavBar from "@/components/navbar";
-import FooterM from "@/components/footer";
-import Layout from "@/components/layout";
 
 export default function Home(props: any) {
   return (
