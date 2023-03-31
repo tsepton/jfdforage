a529b3afe119b612cf2511e4d3df260ca944ce57
Thibaut Septon <tsepton@gmail.com>
2023-03-31 08:02:01 +0200

    Remove wrong import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
-import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Head } from "next/document";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
