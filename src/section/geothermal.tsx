96c0d3fe41b3e2a4074bd47849a681ae4f06a89a
Thibaut Septon <tsepton@gmail.com>
2023-04-05 08:39:50 +0200

    Add (raw) content

--- src/section/geothermal.tsx
+++ src/section/geothermal.tsx
@@ -1,10 +1,24 @@
 import SectionCard from "@/components/section_card";
+import trans from "@/translations/translator";
 import Section from "../components/section";
 
-export default function GeothermalSection(props: any) {
+export default function PhotoSection(props: any) {
+
   return (
     <Section id={props.id} className={props.className}>
-      <SectionCard>Geothermal</SectionCard>
+      <SectionCard>
+        <h1 className="text-left text-3xl lg:text-4xl font-light text-gray-900 dark:text-white mb-5 lg:mb-10">
+          {trans.get("sections.geothermal.title")}
+        </h1>
+        {trans.get("sections.geothermal.content1")}
+        {trans.get("sections.geothermal.content2")}
+        {trans.get("sections.geothermal.subtitle1")}
+        {trans.get("sections.geothermal.content3")}
+        {trans.get("sections.geothermal.content4")}
+        {trans.get("sections.geothermal.subtitle2")}
+        {trans.get("sections.geothermal.advantages")}
+        {trans.get("sections.geothermal.conclusion")}
+      </SectionCard>
     </Section>
   );
 }
