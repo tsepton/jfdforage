80096e66ebeba5b05552caeaab5d0460faf70645
Thibaut Septon <tsepton@gmail.com>
2023-04-05 19:53:53 +0200

    Deploy latest version

--- out/_next/static/muN3UHz7-WtnpxSXmmR96/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-bbcf856a094e7ccd.js","static/chunks/904-f3c1d1468915aa13.js","static/chunks/pages/index-29d104b71f3d29cb.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
