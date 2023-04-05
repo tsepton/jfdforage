073e5aea2159dc7302a40307fa51d3eae82a4466
Thibaut Septon <tsepton@gmail.com>
2023-04-05 12:56:18 +0200

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,13 @@ module.exports = {
       colors: {
         jfd: {
           grey: "#373737",
+          "grey-light": "#5C5C5C",
+          "grey-lighter": "#F5F5F5",
           blue: "#066b7f",
+          "blue-light": "#09A4C3",
+          "blue-lighter": "#ECFBFE",
           orange: "#f18f34",
-          "orange-light": "#F4B67B",
+          "orange-light": "#F4AB66",
         },
       },
     },
