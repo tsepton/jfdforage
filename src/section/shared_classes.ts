2bbfd82d5bdd888251007dc9577ff90d77193fec
Thibaut Septon <tsepton@gmail.com>
2023-04-05 16:19:14 +0200

    Create shared classes to be shared accross sections

---
+++ src/section/shared_classes.ts
@@ -0,0 +1,4 @@
+export const title = "text-left text-3xl md:text-4xl font-light text-gray-900 dark:text-white mb-5 lg:mb-10 text-shadow-sm shadow-slate-400";
+export const subtitle = "text-xl md:text-2xl text-gray-900 font-light dark:text-white mb-3 md:mb-5 text-shadow-sm shadow-slate-400";
+export const content = "text-gray-900 dark:text-white font-normal";
+export const ending = "text-lg md:text-xl font-light text-gray-900 text-light dark:text-white text-shadow-sm shadow-slate-300";
