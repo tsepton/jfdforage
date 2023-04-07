47973e67199ccf8563d2452640942ec39db86103
Thibaut Septon <tsepton@gmail.com>
2023-04-07 15:01:25 +0200

    Add styling to content

--- src/section/services.tsx
+++ src/section/services.tsx
@@ -1,27 +1,40 @@
 import SectionCard from "@/components/section_card";
 import Section from "../components/section";
 import trans from "@/translations/translator";
-import { content, title } from "./shared_classes";
+import { content, subtitle, title } from "./shared_classes";
+import FooterSection from "@/components/footer_section";
 
 export default function ServiceSection(props: any) {
-  const contentTemp = true
-    ? trans.get("sections.photo.content")
-    : <div>
-      {trans.get("sections.service.motivation")}
-      {trans.get("sections.service.endMotivation")}
-      {trans.get("sections.service.subtitle")}
-      {trans.get("sections.service.services")}
-
-    </div>;
   return (
     <Section id={props.id} className={props.className}>
       <SectionCard>
-        <h1 className={title}>
-          {trans.get("sections.service.title")}
-        </h1>
-        <div className={content}>
-          {contentTemp}
+        <h1 className={title}>{trans.get("sections.service.title")}</h1>
+        <div className="flex flex-col justify-start items-start text-left">
+          <div className={content}>
+            {trans.get("sections.service.motivation")}
+          </div>
+          <div className={content}>
+            {trans.get("sections.service.endMotivation")}
+          </div>
+
+          <div className={subtitle + " mt-10"}>
+            {trans.get("sections.service.subtitle")}
+          </div>
+          <div className={content}>
+            {(trans.get("sections.service.services") as string[]).map(
+              (elem, i) => (
+                <div key={i}> - {elem}</div>
+              )
+            )}
+          </div>
         </div>
+        <FooterSection
+          className={"pt-[3em]"}
+          currentSection={"#geothermal"}
+          moreInfo={true}
+          showContact={true}
+          showText={false}
+        />
       </SectionCard>
     </Section>
   );
