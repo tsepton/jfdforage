a46ec39529851d8dbf4a5a795d33030b1cb42601
Thibaut Septon <tsepton@gmail.com>
2023-04-07 15:01:30 +0200

    Format code

--- src/section/geothermal.tsx
+++ src/section/geothermal.tsx
@@ -5,14 +5,10 @@ import { title, content, subtitle, highlight } from "./shared_classes";
 import FooterSection from "@/components/footer_section";
 
 export default function PhotoSection(props: any) {
-
-
   return (
     <Section id={props.id} className={props.className}>
       <SectionCard>
-        <h1 className={title}>
-          {trans.get("sections.geothermal.title")}
-        </h1>
+        <h1 className={title}>{trans.get("sections.geothermal.title")}</h1>
         <div className="flex flex-col justify-start items-start text-left">
           <div className={content}>
             {trans.get("sections.geothermal.content1")}
@@ -35,14 +31,23 @@ export default function PhotoSection(props: any) {
             {trans.get("sections.geothermal.subtitle2")}
           </div>
           <div className={content}>
-            {(trans.get("sections.geothermal.advantages") as string[])
-              .map((elem, i) => <div key={i}> - {elem}</div>)}
+            {(trans.get("sections.geothermal.advantages") as string[]).map(
+              (elem, i) => (
+                <div key={i}> - {elem}</div>
+              )
+            )}
           </div>
           <div className={highlight + " pt-5"}>
             {trans.get("sections.geothermal.conclusion")}
           </div>
         </div>
-        <FooterSection className={'pt-[3em]'} currentSection={'#geothermal'} moreInfo={true} showContact={true} showText={false}></FooterSection>
+        <FooterSection
+          className={"pt-[3em]"}
+          currentSection={"#geothermal"}
+          moreInfo={true}
+          showContact={true}
+          showText={false}
+        ></FooterSection>
       </SectionCard>
     </Section>
   );
