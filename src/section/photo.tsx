44ed083a40bcbedd1975f5fc123a8fd195b5bd98
tsepton <tsepton@gmail.com>
2023-10-30 11:35:29 +0100

    Add wip for photo gallery

--- src/section/photo.tsx
+++ src/section/photo.tsx
@@ -6,21 +6,83 @@ import Section from "../components/section";
 import static_asset_listing from "../gallerie_files.json";
 import { content as contentClass, title } from "./shared_classes";
 import FooterSection from "@/components/footer_section";
+import { useState } from "react";
 
 export default function PhotoSection(props: any) {
+  // TODO - refactor this mess
+
   const images = static_asset_listing.map((file) => {
     const uri = `/img/gallerie/${file}`;
-    return <img key={uri} src={uri} alt="photo chantier" />;
+    return (
+      <img
+        onClick={(e) => showFullScreen(e)}
+        key={uri}
+        src={uri}
+        alt="Photo chantier"
+        style={{
+          cursor: "pointer",
+        }}
+      />
+    );
   });
 
+  const [selected, setSelectedImg] = useState(undefined);
+
+  const showFullScreen = (e) => {
+    setSelectedImg(e.target);
+  };
+
+  const hideFullScreen = () => {
+    setSelectedImg(undefined);
+  };
+
+  const fullScreenContent = (
+    <div
+      className={contentClass + "pb-[3em]"}
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        zIndex: 100,
+        top: 0,
+        left: 0,
+        background: "rgb(0,0,0, 0.5)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: "pointer",
+      }}
+      onClick={hideFullScreen}
+    >
+      <img
+        style={{
+          height: "75vh",
+          width: "75vw",
+          objectFit: "scale-down",
+        }}
+        src={selected?.currentSrc}
+        alt="Photo chantier"
+      ></img>
+    </div>
+  );
+  const previewContent = (
+    <div
+      className={contentClass + "pb-[3em]"}
+      style={{ height: "50vh !important" }}
+    >
+      <Carousel>{images}</Carousel>
+    </div>
+  );
+
   return (
-    <Section id={props.id} className={props.className}>
-      <SectionCard>
-        {/* <h1 className={title}>{trans.get("sections.photo.title")}</h1> */}
-        <div className={contentClass + "pb-[3em]"} style={{ height: "100% !important" }}>
-          <Carousel>{images}</Carousel>
-        </div>
-      </SectionCard>
-    </Section>
+    <>
+      <Section id={props.id} className={props.className}>
+        <SectionCard>
+          <h1 className={title}>{trans.get("sections.photo.title")}</h1>
+          {previewContent}
+        </SectionCard>
+      </Section>
+      {!!selected && fullScreenContent}
+    </>
   );
 }
