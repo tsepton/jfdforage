843ee85c9c195df08aeed4f4b6aae9ff19f4afd8
Thibaut Septon <tsepton@gmail.com>
2024-09-04 16:25:23 +0200

    Move button position on menu open

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,6 +23,7 @@ export default function NavBar(props: any) {
   }, [currentSection]);
 
   useEffect(() => {
+    // FIXME - does not work on page reload
     const updateOnUrlChange = () =>
       updateSection(undefined, window.location.href);
     window.addEventListener("popstate", updateOnUrlChange);
@@ -40,21 +41,36 @@ export default function NavBar(props: any) {
   };
 
   return (
-    <Navbar
-      id={process.env.navbarId}
-      className={props.className + " sticky top-0 w-full shadow-md "}
-      fluid={true}
-      rounded={true}
-      style={{ zIndex: "100" }}
-    >
-      <div>
-        <a href="#home">
-          <img
-            src="/img/logo.jpg"
-            className="mr-3 h-6 sm:h-9"
-            alt="JFD forage Logo"
-          />
+    <Navbar id={process.env.navbarId} fluid rounded style={{ zIndex: "100" }}>
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img
+          src="/img/logo.jpg"
+          className="mr-3 h-6 sm:h-9"
+          alt="JFD forage Logo"
+        />
+      </Navbar.Brand>
+
+      <div className="flex md:order-2 ">
+        <a
+          href="#contact"
+          className="inline-flex justify-center items-center py-2 px-3 text-base text-center text-white bg-jfd-orange hover:bg-jfd-grey focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+        >
+          {trans.get("sections.getInTouch")}
+          <svg
+            aria-hidden="true"
+            className="ml-2 -mr-1 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
         </a>
+        <Navbar.Toggle />
       </div>
 
       <Navbar.Collapse className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
@@ -94,29 +110,6 @@ export default function NavBar(props: any) {
           {trans.get("navbar.pic")}
         </a>
       </Navbar.Collapse>
-
-      <div className="flex md:order-2 p-0">
-        <a
-          href="#contact"
-          className="inline-flex justify-center items-center py-2 px-3 text-base text-center text-white bg-jfd-orange hover:bg-jfd-grey focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
-        >
-          {trans.get("sections.getInTouch")}
-          <svg
-            aria-hidden="true"
-            className="ml-2 -mr-1 w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </a>
-        <Navbar.Toggle />
-      </div>
     </Navbar>
   );
 }
